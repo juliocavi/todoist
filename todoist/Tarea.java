c012e8d6419df6b1197d898fcd141a689d9477c1
Julio Cachon Villadangos <juliocachonvi@gmail.com>
2017-12-20 13:33:25 +0100

    Refactorizacion Todoist

---
+++ todoist/Tarea.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Tarea
+{
+    //definimos array de todoist
+    private ArrayList<Todoist> tarea;
+    
+    //creamos objeto todoist
+    public Tarea()
+    {
+        tarea = new ArrayList<Todoist>();
+    }
+        
+    //ahora creamos tares con el metodo addTarea de todoist
+}
