0c9940a256ac98cbbc57c2cab83f17e9f45a23e0
Julio Cachon Villadangos <juliocachonvi@gmail.com>
2018-01-12 10:27:43 +0100

    TareaConMayorPrioridad.v1

--- todoist/Tarea.java
+++ todoist/Tarea.java
@@ -1,15 +1,88 @@
-import java.util.ArrayList;
+/**
+ * Crea un objeto que almacena una tarea y su estado (completada o no).
+ * 
+ * @author 1ºDAM 2017/2018
+ * @version 10/11/2018
+ */
 
-public class Tarea
+public class Tarea 
 {
-    //definimos array de todoist
-    private ArrayList<Todoist> tarea;
-    
-    //creamos objeto todoist
-    public Tarea()
+    private String descripcion;
+    // El estado de la tarea: True: Completada; False: No Completada. 
+    private boolean estado;
+    private int prioridad;
+
+    /**
+     * Constructor de la clase Tarea.
+     * @param descripcion La descripción de la tarea
+     * Sergio
+     */
+    public Tarea(String descripcion)
     {
-        tarea = new ArrayList<Todoist>();
+        this.descripcion = descripcion;
+        estado = false;
+        prioridad = 1;
+
     }
-        
-    //ahora creamos tares con el metodo addTarea de todoist
+
+    /**
+     * Devuelve si el estado es true o false.
+     */
+    public boolean getEstado()
+    {
+        return estado;  
+    }
+
+    /**
+     * Devuelve la descripción de la tarea.
+     */
+    public String getDescripcion()
+    {
+        return descripcion;
+    }
+    
+    /**
+     * Devolver prioridad de la tarea
+     */
+    public int getPrioridad()
+    {
+        return prioridad;
+    }
+    
+    /**
+     * Devuelve toda la informacion de la tarea en formato String
+     */
+    public String getDatosTarea()
+    {
+        String aDevolver = descripcion + " Prioridad: " + prioridad;
+        if (estado) {
+            aDevolver = "[X] " + aDevolver;
+        }
+        else {
+            aDevolver = "[ ] " + aDevolver;
+        }
+        return aDevolver; 
+    }
+
+    
+    
+    /**
+     * Metodos setter
+     * Javier y Jorge
+     */
+    public void tareaCompletada() {
+        estado = true;
+    }
+    
+    /**
+     * Cambiar prioridad de la tarea
+     */
+    
+    public void setPrioridad(int prioridad){
+        if (prioridad >= 1 && prioridad <= 5){
+            this.prioridad = prioridad;
+        }
+    }
 }
+
+
---
+++ todoist/Todoist2.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+/** 
+ * Cabecera: luis
+ * Atributos: Gian carlo
+ * Constructor: Aitor
+ * mostrarTareasNumeradas: Samuel
+ * agregarTarea: Omar
+ * marcarTareaComoCompletada: Cristian
+ **/
+
+public class Todoist2{
+
+    private ArrayList<Tarea> listaDeTareas; 
+
+    /**
+     * Constructor de la clase Todoist2
+     */
+    public Todoist2(){
+        listaDeTareas = new ArrayList<Tarea>();
+    }
+
+    /**
+     * Mostrar tareas numeradas
+     */
+    public void mostrarTareasNumeradas()
+    {
+        int posicionTareaActual = 0;
+        while (posicionTareaActual < listaDeTareas.size()) {
+            System.out.println((posicionTareaActual+1) + ". " + listaDeTareas.get(posicionTareaActual).getDatosTarea());
+            posicionTareaActual++;
+        }
+    }
+
+    /**
+     * Añade una tarea
+     */
+    public void addTarea(String tarea)
+    {
+        Tarea nuevaTarea = new Tarea(tarea);
+        listaDeTareas.add(nuevaTarea);
+
+    }
+
+    /**
+     * Marca como completada la tarea indicada como parametro. Por ejemplo,
+     * si el parámetro es 0 marca como completada la primera tarea, si es 1 la
+     * segunda, etc.
+     */
+    public void marcarComoCompletada(int indiceTarea)
+    {
+        Tarea tareaActual = listaDeTareas.get(indiceTarea);
+        tareaActual.tareaCompletada();
+        //listaDeTareas.set(indiceTarea, tareaActual);
+        //ESTAS LINEAS SOBRAN, YA QUE CON LA ANTERIOR YA ESTAMOS MODIFICANDO LA TAREA, AL TRATARSE DE UNA TAREA REFERENCIADA
+    }
+
+    /**
+     * Cambia la prioridad de la tarea indicada.Por ejemplo,
+     * si el parámetro es 0 cambia la prioridad de la primera tarea, si es 1 la
+     * segunda, etc. Se asume que se indica una tarea valida.
+     */
+
+    public void cambiarPrioridad(int indiceTarea, int nuevaPrioridad)
+    {
+        if (indiceTarea>=0 && indiceTarea<listaDeTareas.size()){
+            Tarea tareaActual = listaDeTareas.get(indiceTarea);
+            tareaActual.setPrioridad(nuevaPrioridad);
+            //listaDeTareas.set(indiceTarea, tareaActual);
+        }
+    }
+
+    /**
+     * Imprime todos los datos de la tarea con mayor prioridad. Si hay empate,
+     * imprime la última encontrada. Si no hay tareas no imprime nada.
+     */
+
+    public void tareaConMayorPrioridad()
+    {
+        if (listaDeTareas.size() > 0){
+            Tarea tareaPrioridadMaxima = listaDeTareas.get(0);
+            int prioridadMaxima = 0;
+            for (Tarea tareaActual : listaDeTareas){
+                if (tareaActual.getPrioridad() >= prioridadMaxima){
+                    tareaPrioridadMaxima = tareaActual;
+                    prioridadMaxima = tareaActual.getPrioridad();
+                }
+            }
+            System.out.println(tareaPrioridadMaxima.getDatosTarea());
+        }
+    }
+}
